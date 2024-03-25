6282bbf5d78f5f728028423a545a416b49caf992
Wonjun <ky1429@naver.com>
2024-03-25 20:34:02 +0900

    second

--- baekjoon/1001/1001.c
+++ baekjoon/1001/1001.c
@@ -2,5 +2,7 @@
 
 int main(void)
 {
-    printf("test \n");  
+    printf("int 자료형의 크기: %ld바이트 \n", sizeof(int));
+    printf("float 자료형의 크기: %ld바이트 \n", sizeof(float));
+    printf("char 자료형의 크기: %ld바이트 \n", sizeof(char));
 }
